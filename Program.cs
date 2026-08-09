df222f0d387a2ff6a46b832effa3d56c0f27ad8f
antonyessac351-debug <antonyessac@gmail.com>
2026-08-10 00:26:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Smart_Delivery_Management_System_OOP_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
